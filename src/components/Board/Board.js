ba5e32d786b0fc57941621d1307e76b3f6d24af6
zeyadetman <zeyadetman@gmail.com>
2019-01-25 07:02:03 +0200

    It works!

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { levels } from '../../utils/levels';
+import React, { Component } from "react";
+import { levels } from "../../utils/levels";
 
 export default class Board extends Component {
   constructor(props) {
@@ -7,37 +7,60 @@ export default class Board extends Component {
     this.state = {
       board: "",
       boardId: "",
-      painter: ""
-    }
+      painter: "",
+      isBoardSolved: false
+    };
 
     this.drawBoard = this.drawBoard.bind(this);
     this.cellCheck = this.cellCheck.bind(this);
+    this.isSolved = this.isSolved.bind(this);
   }
 
   componentWillReceiveProps(nextProps, nextContext) {
-    this.setState({
-      board: nextProps.label,
-      boardId: Math.floor(Math.random() * (levels[nextProps.label] ? levels[nextProps.label].length : 0))
-    }, this.drawBoard);
+    this.setState(
+      {
+        board: nextProps.label,
+        boardId: Math.floor(
+          Math.random() *
+            (levels[nextProps.label] ? levels[nextProps.label].length : 0)
+        )
+      },
+      this.drawBoard
+    );
   }
 
   componentDidMount() {
-    this.setState({
-      board: this.props.label,
-      boardId: Math.floor(Math.random() * (levels[this.props.label] ? levels[this.props.label].length : 0))
-    }, this.drawBoard);
+    this.setState(
+      {
+        board: this.props.label,
+        boardId: Math.floor(
+          Math.random() *
+            (levels[this.props.label] ? levels[this.props.label].length : 0)
+        )
+      },
+      this.drawBoard
+    );
   }
-
 
   drawBoard() {
     let indx = 0;
-    const levelCount = ++this.props.label.split('x')[0];
+    const levelCount = ++this.props.label.split("x")[0];
     let painter = "";
     for (let row = 0; row < levelCount; row++) {
-      painter += "<tr>"
+      painter += "<tr>";
       for (let col = 0; col < levelCount; col++) {
-        painter += `<td id=${row}-${col} class="empty" style="${row === 1 && col === 0 ? 'width: 100px' : ''}">
-    ${(!row && !col) ? "" : (!row || !col) ? levels[this.props.label][this.state.boardId][indx++].toString().replace(/,/g, '') : ''}
+        painter += `<td id=${row}-${col} class="empty" style="${
+          row === 1 && col === 0 ? "width: 100px" : ""
+        }">
+    ${
+      !row && !col
+        ? ""
+        : !row || !col
+        ? levels[this.props.label][this.state.boardId][indx++]
+            .toString()
+            .replace(/,/g, "")
+        : ""
+    }
   </td>`;
       }
       painter += "</tr>";
@@ -48,12 +71,44 @@ export default class Board extends Component {
 
   cellCheck(e) {
     const cellId = e.target.id;
-    const cellClass = document.getElementById(cellId) && document.getElementById(cellId).classList.value;
+    const cellClass =
+      document.getElementById(cellId) &&
+      document.getElementById(cellId).classList.value;
 
-    if (cellClass && !cellId.split('-').some(el => el === '0') && cellId !== 'board') {
-      document.getElementById(cellId).classList.value = (cellClass === 'empty') ? 'doubtful' : (cellClass === 'doubtful') ? 'fill' : (cellClass === 'fill') ? 'empty' : 'empty';
-      console.log(cellId, document.getElementById(cellId).classList.value);
+    if (
+      cellClass &&
+      !cellId.split("-").some(el => el === "0") &&
+      cellId !== "board"
+    ) {
+      document.getElementById(cellId).classList.value =
+        cellClass === "empty"
+          ? "doubtful"
+          : cellClass === "doubtful"
+          ? "fill"
+          : cellClass === "fill"
+          ? "empty"
+          : "empty";
     }
+
+    this.setState({isBoardSolved: this.isSolved()}, ()=>console.log(this.state.isBoardSolved));
+  }
+
+  isSolved() {
+    const { board, boardId } = this.state;
+
+    let filledCells = [].slice.call(document.getElementsByClassName('fill'));
+    filledCells = filledCells.map(e => e.id);
+    const boardCols = +board.split('x')[0];
+    const boardRows = +board.split('x')[1];
+    const levelSummed = levels[board][boardId].map(e => e.reduce((prev, cur) => prev + cur, 0))
+    const res = new Array(boardCols+boardRows).fill(0);
+    filledCells.forEach(cell => {
+      const id = cell.split('-');
+      res[--(id[0]) + boardCols]++;
+      res[--(id[1])]++;
+    })
+
+    return res.every((val, indx) => val === levelSummed[indx]);
   }
 
   render() {
@@ -62,9 +117,8 @@ export default class Board extends Component {
         id="board"
         onClick={this.cellCheck}
         dangerouslySetInnerHTML={{ __html: this.state.painter }}
-        style={{ margin: '0 auto', border: 'none' }}
-      >
-      </table>
-    )
+        style={{ margin: "0 auto", border: "none" }}
+      />
+    );
   }
 }
