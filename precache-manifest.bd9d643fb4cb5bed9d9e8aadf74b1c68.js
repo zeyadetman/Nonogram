1bd70dbc3e9201e8f51bb981e616be4a96127f45
zeyadetman <zeyadetman@gmail.com>
2019-01-25 08:14:06 +0200

    Updates

---
+++ precache-manifest.bd9d643fb4cb5bed9d9e8aadf74b1c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "878dc9e8b9435767565c",
+    "url": "/Nonogram/static/js/runtime~main.878dc9e8.js"
+  },
+  {
+    "revision": "0c68f918453705d28e85",
+    "url": "/Nonogram/static/js/main.0c68f918.chunk.js"
+  },
+  {
+    "revision": "110690f685462602754d",
+    "url": "/Nonogram/static/js/1.110690f6.chunk.js"
+  },
+  {
+    "revision": "0c68f918453705d28e85",
+    "url": "/Nonogram/static/css/main.2cf0394f.chunk.css"
+  },
+  {
+    "revision": "e18fa48c9d73313e27517396903ae604",
+    "url": "/Nonogram/index.html"
+  }
+];
